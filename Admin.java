3eef53771fb2b0963c1af52b6a0f4a5b4f82c90f
k-6672 <101245719+k-6672@users.noreply.github.com>
2022-03-09 09:43:22 +0100

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,26 @@
+package Projekte;
+
+public class Admin {
+
+	private String name;
+	private String email;
+	private String password;
+	
+	public Admin (String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getEmail() {
+		return this.email;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+
+
+}
